039cf30dbded730809c3ff10f34ce2387cd62f5c
balakumar-s <s.balakumar@outlook.com>
2013-05-30 20:27:14 +0530

    added testBuild.cpp building executables tested

---
+++ build/CMakeFiles/testBuild.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testBuild.dir/src/testBuild.cpp.o"
+  "../bin/testBuild.pdb"
+  "../bin/testBuild"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testBuild.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/jarvis/ROS_jarvis/rosbuildFiles/ROSpcl/build/devel/setup.sh"
+emulate zsh # back to zsh mode
